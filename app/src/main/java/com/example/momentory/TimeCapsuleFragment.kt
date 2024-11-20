2f938574db12baa8e0cf489eb4c99296a06f0666
hhhbbs1028 <hhhbbs1028@sookmyung.ac.kr>
2024-11-20 22:02:28 +0900

    merge

--- app/src/main/java/com/example/momentory/TimeCapsuleFragment.kt
+++ app/src/main/java/com/example/momentory/TimeCapsuleFragment.kt
@@ -7,22 +7,29 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import android.content.Intent
 import android.widget.ImageView
+import com.example.momentory.databinding.FragmentTimeCapsuleBinding
 
 
 class TimeCapsuleFragment : Fragment() {
+    private var _binding: FragmentTimeCapsuleBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_time_capsule, container, false)
+        _binding = FragmentTimeCapsuleBinding.inflate(inflater, container, false)
 
-        // 캡슐 생성
-        val createCapsuleButton: ImageView = view.findViewById(R.id.create_capsule_button)
-        createCapsuleButton.setOnClickListener {
+        binding.createCapsuleButton.setOnClickListener {
             val intent = Intent(activity, CreateCapsuleActivity::class.java)
             startActivity(intent)
         }
 
-        return view
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
