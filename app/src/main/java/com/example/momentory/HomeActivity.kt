58effad52c8e974d7d12bb1213603fe4d93daf13
jaeee25 <limjaeseo@sookmyung.ac.kr>
2024-11-20 21:35:39 +0900

    1120

--- app/src/main/java/com/example/momentory/HomeActivity.kt
+++ app/src/main/java/com/example/momentory/HomeActivity.kt
@@ -15,20 +15,19 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 툴바 설정 (기본 타이틀 비활성화)
         setSupportActionBar(binding.toolbar)
-        supportActionBar?.setDisplayShowTitleEnabled(false) // 기본 타이틀 비활성화
+        supportActionBar?.setDisplayShowTitleEnabled(false)
 
-        // ViewPager 설정
+
         val adapter = ViewPagerAdapter(this)
         binding.viewPager.adapter = adapter
 
-        // TabLayout 연결
+
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
-            // View Binding을 사용해 커스텀 탭 레이아웃 초기화
+
             val tabBinding = CustomTabBinding.inflate(layoutInflater)
 
-            // 탭 텍스트 설정
+
             tabBinding.tabText.text = when (position) {
                 0 -> "공유일기"
                 1 -> "비밀일기"
@@ -36,10 +35,10 @@ class HomeActivity : AppCompatActivity() {
                 else -> ""
             }
 
-            // 탭의 커스텀 뷰로 설정
+
             tab.customView = tabBinding.root
         }.attach()
-        // 프로필 이미지 클릭 이벤트
+
         binding.profileImage.setOnClickListener {
             val intent = Intent(this, ProfileEditActivity::class.java)
             startActivity(intent)
