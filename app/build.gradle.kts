3e5a210d5f1c77fa66d3710fe32707dedf561d54
hhhbbs1028 <hhhbbs1028@sookmyung.ac.kr>
2024-12-03 22:51:59 +0900

    gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
